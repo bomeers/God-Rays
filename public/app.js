cb38781cb79287237bf304a233d4a65e10e7ad27
bo meers <bmeers5@gmail.com>
2022-03-28 20:55:41 -0500

    initial commit

--- public/app.js
+++ public/app.js
@@ -1,31 +1,31 @@
-// let ioConnection = new io();
+let ioConnection = new io();
 
 let viewerCount = 0;
 let likeCount = 0;
 let diamondsCount = 0;
 
-// $(document).ready(() => {
-//     $('#connectButton').click(connectTTL);
-//     $('#uniqueIdInput').on('keyup', function (e) {
-//         if (e.key === 'Enter') {
-//             connectTTL();
-//         }
-//     });
-// })
+$(document).ready(() => {
+    $('#connectButton').click(connect);
+    $('#uniqueIdInput').on('keyup', function (e) {
+        if (e.key === 'Enter') {
+            connect();
+        }
+    });
+})
 
-// function connectTTL() {
-//     let uniqueId = $('#uniqueIdInput')?.val();
-//     if (uniqueId !== '') {
-//         ioConnection.emit('setUniqueId', uniqueId, {
-//             enableExtendedGiftInfo: true
-//         });
-//         $('#stateText').text('Connecting...');
-//     } else {
-//         alert('no username entered');
-//     }
-// }
+function connect() {
+    let uniqueId = $('#uniqueIdInput').val();
+    if (uniqueId !== '') {
+        ioConnection.emit('setUniqueId', uniqueId, {
+            enableExtendedGiftInfo: true
+        });
+        $('#stateText').text('Connecting...');
+    } else {
+        alert('no username entered');
+    }
+}
 
-function sanitize(text: string) {
+function sanitize(text) {
     return text.replace(/</g, '&lt;')
 }
 
@@ -33,15 +33,15 @@ function updateRoomStats() {
     $('#roomStats').html(`Viewers: <b>${viewerCount.toLocaleString()}</b> Likes: <b>${likeCount.toLocaleString()}</b> Earned Diamonds: <b>${diamondsCount.toLocaleString()}</b>`)
 }
 
-function generateUsernameLink(data: any) {
+function generateUsernameLink(data) {
     return `<a class="usernamelink" href="https://www.tiktok.com/@${data.uniqueId}" target="_blank">${data.uniqueId}</a>`;
 }
 
-function isPendingStreak(data: any) {
+function isPendingStreak(data) {
     return data.gift.gift_type === 1 && data.gift.repeat_end === 0;
 }
 
-function addChatItem(color: string, data: any, text: string, summarize: any) {
+function addChatItem(color, data, text, summarize) {
     let container = $('.chatcontainer');
 
     if (container.find('div').length > 500) {
@@ -66,8 +66,7 @@ function addChatItem(color: string, data: any, text: string, summarize: any) {
     }, 400);
 }
 
-function addGiftItem(data: any) {
-    //TODO find way to use this without jquery
+function addGiftItem(data) {
     let container = $('.giftcontainer');
 
     if (container.find('div').length > 200) {
@@ -111,6 +110,74 @@ function addGiftItem(data: any) {
     }, 800);
 }
 
-export default {
-    // connectTTL
-}
+// Control events
+ioConnection.on('setUniqueIdSuccess', (state) => {
+    // reset stats
+    viewerCount = 0;
+    likeCount = 0;
+    diamondsCount = 0;
+    updateRoomStats();
+    $('#stateText').text(`Connected to roomId ${state.roomId}`);
+})
+
+ioConnection.on('setUniqueIdFailed', (errorMessage) => {
+    $('#stateText').text(errorMessage);
+})
+
+ioConnection.on('streamEnd', () => {
+    $('#stateText').text('Stream ended.');
+})
+
+// viewer stats
+ioConnection.on('roomUser', (msg) => {
+    if (typeof msg.viewerCount === 'number') {
+        viewerCount = msg.viewerCount;
+        updateRoomStats();
+    }
+})
+
+// like stats
+ioConnection.on('like', (msg) => {
+    if (typeof msg.likeCount === 'number') {
+        addChatItem('#447dd4', msg, msg.label.replace('{0:user}', '').replace('likes', `${msg.likeCount} likes`))
+    }
+
+    if (typeof msg.totalLikeCount === 'number') {
+        likeCount = msg.totalLikeCount;
+        updateRoomStats();
+    }
+})
+
+// Chat events,
+let joinMsgDelay = 0;
+ioConnection.on('member', (msg) => {
+    let addDelay = 250;
+    if (joinMsgDelay > 500) addDelay = 100;
+    if (joinMsgDelay > 1000) addDelay = 0;
+
+    joinMsgDelay += addDelay;
+
+    setTimeout(() => {
+        joinMsgDelay -= addDelay;
+        addChatItem('#21b2c2', msg, 'joined', true);
+    }, joinMsgDelay);
+})
+
+ioConnection.on('chat', (msg) => {
+    addChatItem('', msg, msg.comment);
+})
+
+ioConnection.on('gift', (data) => {
+    addGiftItem(data);
+
+    if (!isPendingStreak(data) && data.extendedGiftInfo.diamond_count > 0) {
+        diamondsCount += (data.extendedGiftInfo.diamond_count * data.gift.repeat_count);
+        updateRoomStats();
+    }
+})
+
+// share, follow
+ioConnection.on('social', (data) => {
+    let color = data.displayType.includes('follow') ? '#ff005e' : '#2fb816';
+    addChatItem(color, data, data.label.replace('{0:user}', ''));
+})
